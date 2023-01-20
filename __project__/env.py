90798caf26af2a5c3a236f701c96c28e38e0777e
Arthur Henrique <Arthur.Henrique.Della.Fraga@gmail.com>
2023-01-20 14:23:40 -0300

    feat: Configure django to consume configuration from environment

---
+++ __project__/env.py
@@ -0,0 +1,33 @@
+"""
+Project environment management.
+Configure application expected env variables' type and default values,
+madiating their consuption and improving this handling.
+See https://django-environ.readthedocs.io/en/latest/index.html.
+"""
+
+import environ
+
+from django.core.management.utils import get_random_secret_key
+
+from .path import *
+
+
+# Define expected env variables, its casting and default value
+# See https://django-environ.readthedocs.io/en/latest/tips.html.
+env = environ.Env(
+    ENV_FILE=(str, '.env.example'),
+
+    SECRET_KEY=(str, get_random_secret_key()),
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list, ['*']),
+)
+
+# Manage chosen .env file consumption.
+env.read_env(
+    env('ENV_FILE'),
+    # overwrite=True,
+)
+
+# Extra options
+# env.prefix = 'DJANGO_'
+# env.escape_proxy = True
---
+++ __project__/path.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+# Project path record
+PROJECT_DIR = Path(__file__).resolve().parent
+ROOT_DIR = PROJECT_DIR.parent
